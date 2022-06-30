02990ea11b84aa21014e650bec06e1c79437457a
kevin <kgleijm@gmail.com>
2022-06-30 18:44:14 +0200

    ADD genatic algorithm

--- main.py
+++ main.py
@@ -4,12 +4,11 @@ from videophy import Stream
 from PIL import Image, ImageDraw
 import keyboard
 from GeneticAlgorithm import GeneticAlgorithm
+import math
 
 
-# choose
-# observation: distance sensor (4-8* percentage of max distance) = [sens1, sens2, sens3, sens4]
-# options: -1|+1(up, right, down, left) nothing
-# possibilities: whether you're at max or min speed
+global_Show = False
+
 
 class SimpleMatrix:
 
@@ -56,7 +55,11 @@ class SimpleMatrix:
         return img
 
     def ifPixel(self, x, y):
-        return self[x, y]
+        try:
+            return self[x, y]
+        except:
+            print("#### ifPIxel out of bounds, returning True")
+            return True
 
 
 class Helpers:
@@ -68,7 +71,7 @@ class Helpers:
 
 class Sim:
 
-    def __init__(self, onTick: Callable, imagePath: str):
+    def __init__(self, onTick: Callable, imagePath: str, genome=None):
 
         """
         onTick applies xSpeed and ySpeed to position
@@ -77,11 +80,14 @@ class Sim:
         """
 
         # Open the image form working directory
+        self.genome = genome
         self.onTick = onTick
         self.imagePath = imagePath
         self.image: Image = Image.open(self.imagePath)
 
         self.maxSpeed = 3
+        self.startingX = 450
+        self.startingY = 550
 
         self.xSpeed = 0
         self.ySpeed = 0
@@ -137,20 +143,29 @@ class Sim:
                 if coords in lastTiles[:-1]:
                     self.score -= 1
                 else:
-                    self.score += 1
+                    #adds small bonus for staying close to the tile centers
+                    centerTileScore = max((50 - abs(self.xPos) % 50), (50 - abs(self.yPos) % 50))/100
+                    self.score += 1 + centerTileScore
 
                 lastTiles.append(self.getTileCoords())
+
+            if self.score < 1:
+                xDist = abs(self.xPos - self.startingX)
+                yDist = abs(self.yPos - self.startingY)
+                pythagoreanDistance = math.sqrt(xDist**2 + yDist**2)/100
+                self.score = min(pythagoreanDistance, 0.99)
+
 
             if len(lastTiles) >= 5:
                 lastTiles.pop(0)
 
             # Check whether or not state is faulty
-            if self.matrix.ifPixel(self.xPos, self.yPos) or ticks > maxTicks:
+            if self.matrix.ifPixel(self.xPos, self.yPos) or ticks > maxTicks or (ticks > maxTicks/10 and self.score < 0.05):
                 return self.score
 
             ticks += 1
 
-    def getObservation4(self):
+    def getObservation4(self) -> list[float, float, float, float]:
         add = lambda a, b: (a[0] + b[0], a[1] + b[1])
 
         stepSize = [(0, 1), (1, 0), (-1, 0), (0, -1)]
@@ -166,6 +181,31 @@ class Sim:
         # calculate pixels as percentage of max
         return [1 - stepAmount[x]/max(self.matrix.width, self.matrix.height) for x in range(len(stepAmount))]
 
+    def getObservation8(self) -> list[float, float, float, float]:
+        add = lambda a, b: (a[0] + b[0], a[1] + b[1])
+
+        stepSize = [(0, 1), (1, 1), (1, 0), (1, -1), (-1, 0), (-1, -1), (0, -1), (-1, 1)]
+        stepAmount = [0, 0, 0, 0, 0, 0, 0, 0]
+
+        # count the amount of empty pixels for each direction
+        for i in range(len(stepAmount)):
+            coords = (self.xPos, self.yPos)
+            while not self.matrix.ifPixel(*coords):
+                stepAmount[i] += 1
+                coords = add(coords, stepSize[i])
+
+        # calculate pixels as percentage of max
+        return [1 - stepAmount[x]/max(self.matrix.width, self.matrix.height) for x in range(len(stepAmount))]
+
+    def getPossibilities(self) -> list[float, float, float, float, float]:
+        return [
+            1 if self.ySpeed > -self.maxSpeed else 0,
+            1 if self.xSpeed < self.maxSpeed else 0,
+            1 if self.ySpeed < self.maxSpeed else 0,
+            1 if self.xSpeed > -self.maxSpeed else 0,
+            1
+        ]
+
 
 def checkManualInput(sim: Sim):
     if keyboard.is_pressed('a'):
@@ -177,24 +217,39 @@ def checkManualInput(sim: Sim):
     if keyboard.is_pressed('s'):
         sim.ySpeed += 0.2 if sim.ySpeed < sim.maxSpeed else 0
 
+
 def checkGAInput(sim: Sim):
-    observation = sim.getObservation4()
 
-    #
-    # print(sim.getObservation4())
+    observation: list[float, float, float, float] = sim.getObservation8()
+    possibilities: list[float, float, float, float, float] = sim.getPossibilities()
+    myChoice = GeneticAlgorithm.HelperFunctions.choose(sim.genome, observation, possibilities)
+    choice = ["w", "d", "s", "a", "n"][myChoice]
 
-def fitnessFunc(genome: list[list[float]], rounds):
+    if choice == 'a':
+        sim.xSpeed -= 0.2 if sim.xSpeed > -sim.maxSpeed else 0
+    if choice == 'd':
+        sim.xSpeed += 0.2 if sim.xSpeed < sim.maxSpeed else 0
+    if choice == 'w':
+        sim.ySpeed -= 0.2 if sim.ySpeed > -sim.maxSpeed else 0
+    if choice == 's':
+        sim.ySpeed += 0.2 if sim.ySpeed < sim.maxSpeed else 0
+
+
+def fitnessFunc4(genome: list[list[float]], rounds=1):
 
     # for each circuit
     totalFitness = 0
     totalRounds = 0
     for circuit in os.listdir("circuits"):
         path = os.path.join("circuits", circuit)
-        print(path)
-        sim = Sim(checkManualInput, path)
+        sim = Sim(checkGAInput, path, genome=genome)
         # run round x times
         for i in range(rounds):
-            fitness = sim.start(show=True)
+            fitness = 0
+            try:
+                fitness = sim.start(show=global_Show)
+            except:
+                print("####ROUND FAILED####") # likely due to getPixel out of range (by sensor observation?)
             print(f"fitness on {path} round {i}: {fitness}")
             sim.reset()
 
@@ -205,9 +260,36 @@ def fitnessFunc(genome: list[list[float]], rounds):
     print(f"Average fitness = {totalFitness/totalRounds}")
     return totalFitness/totalRounds
 
-# GA = GeneticAlgorithm("CircuitDot", fitnessFunc, 4, 5, mutationChance=0.05)
+def fitnessFunc8(genome: list[list[float]], rounds=1):
 
-fitnessFunc(None, 1)
+    # for each circuit
+    totalFitness = 0
+    totalRounds = 0
+    for circuit in os.listdir("circuits"):
+        path = os.path.join("circuits", circuit)
+        sim = Sim(checkGAInput, path, genome=genome)
+        # run round x times
+        for i in range(rounds):
+            fitness = 0
+            try:
+                fitness = sim.start(show=global_Show)
+            except:
+                print("####ROUND FAILED####") # likely due to getPixel out of range (by sensor observation?)
+            print(f"fitness on {path} round {i}: {fitness}")
+            sim.reset()
+
+            # Keep track of score
+            totalFitness += fitness
+            totalRounds += 1
+
+    print(f"Average fitness = {totalFitness/totalRounds}")
+    return totalFitness/totalRounds
+
+# GA = GeneticAlgorithm("CircuitDot", fitnessFunc4, 4, 5, mutationChance=0.2, populationSize=6)
+GA = GeneticAlgorithm("CircuitDot8", fitnessFunc8, 8, 5, mutationChance=0.2, populationSize=6)
+
+GA.run()
+
 
 # sim = Sim(checkManualInput, os.path.join("circuits", "1127809196775976634.png"))
 #
